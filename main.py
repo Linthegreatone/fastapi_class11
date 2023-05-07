493c9bdafe58d0dd2014154f9565e1bc4c375d5a
Linthegreatone <linyao.pan@stonybrook.edu>
2023-05-07 03:33:38 +0000

    completed with fastapi and notations

--- main.py
+++ main.py
@@ -1,39 +1,37 @@
 from fastapi import FastAPI
-import pandas as pd
-
+from starlette.requests import Request #for request function
+from starlette.responses import JSONResponse 
+import pandas as pd 
 
 df = pd.read_csv('./data/diagnoses2019.csv')
-
 
 app = FastAPI()
 
 @app.get('/')
-async def home():
+def home():
     return 'this is a API service for MN ICD code details'
 
-
 @app.get('/preview')
-def preview():
+async def preview():
     top10rows = df.head(1)
     result = top10rows.to_json(orient="records")
     return result
 
-
-@app.get('/icd/<value>')
-def icdcode(value):
+@app.get('/icd/{value}') #flask uses <> Fastapi uses {} for placeholders
+async def icdcode(value):
     print('value: ', value)
     filtered = df[df['principal_diagnosis_code'] == value]
     if len(filtered) <= 0:
         return 'There is nothing here'
-    else:
+    else: 
         return filtered.to_json(orient="records")
 
-
-@app.get('/icd/<value>/sex/<value2>')
-def icdcode2(value, value2):
+@app.get('/icd/{value}/payer/{value2}') #flask uses <> Fastapi uses {} for placeholders
+async def icdcode2(value, value2):
     filtered = df[df['principal_diagnosis_code'] == value]
-    filtered2 = filtered[filtered['sex'] == value2]
+    filtered2 = filtered[filtered['payer'] == value2]
     if len(filtered2) <= 0:
         return 'There is nothing here'
-    else:
-        return filtered2.to_json(orient="records")
+    else: 
+        return filtered2.to_json(orient="records")    
+    #use MEDICARE for payer, R73 for value, icd and payer are lower case
