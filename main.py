7b8febbb28ae53eb67cd77f07b51c759d7816aab
Linthegreatone <linyao.pan@stonybrook.edu>
2023-05-07 02:22:20 +0000

    a

--- main.py
+++ main.py
@@ -1,37 +1,38 @@
-from fastapi import FastAPI 
+from flask import Flask, request 
 import pandas as pd 
 
-df = pd.read_csv('./data/diagnoses2019.csv',
-                 sep='\t', 
-                 names=['code'],
-                 header=None)
+df = pd.read_csv('./data/diagnoses2019.csv')
 
-app = FastAPI()
+app = Flask(__name__)
 
-@app.get('/')
-async def home():
-    return {'this is a API service for MN ICD code details'}
+@app.route('/', methods=["GET"])
+def home():
+    return 'this is a API service for MN ICD code details'
 
-@app.get('/preview')
-async def preview():
+@app.route('/preview', methods=["GET"])
+def preview():
     top10rows = df.head(1)
     result = top10rows.to_json(orient="records")
-    return {result}
+    return result
 
-@app.get('/icd/<value>')
-async def icdcode(value):
+@app.route('/icd/<value>', methods=['GET'])
+def icdcode(value):
     print('value: ', value)
     filtered = df[df['principal_diagnosis_code'] == value]
     if len(filtered) <= 0:
-        return {'There is nothing here'}
+        return 'There is nothing here'
     else: 
-        return {filtered.to_json(orient="records")}
+        return filtered.to_json(orient="records")
 
-@app.get('/icd/<value>/sex/<value2>')
-async def icdcode2(value, value2):
+@app.route('/icd/<value>/sex/<value2>')
+def icdcode2(value, value2):
     filtered = df[df['principal_diagnosis_code'] == value]
     filtered2 = filtered[filtered['sex'] == value2]
     if len(filtered2) <= 0:
-        return {'There is nothing here'}
+        return 'There is nothing here'
     else: 
-        return {filtered2.to_json(orient="records")}
+        return filtered2.to_json(orient="records")    
+    
+
+if __name__ == '__main__':
+    app.run(debug=True)
