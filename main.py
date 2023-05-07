553358fde1ec4ca970ab8d5875b67bf377a13943
Linthegreatone <linyao.pan@stonybrook.edu>
2023-05-07 01:30:16 +0000

    codex done

--- main.py
+++ main.py
@@ -2,8 +2,36 @@ from time import time
 import pandas as pd
 from fastapi import FastAPI, __version__
 
+df = pd.read_csv('./data/diagnoses2019.csv')
+
 app = FastAPI() # This is what will be refrenced in config
 
 @app.get('/')
 async def hello():
     return {'res': 'pong', 'version': __version__, "time": time()}
+
+@app.get('/preview')
+async def preview():
+    top10rows = df.head(1)
+    result = top10rows.to_json(orient="records")
+    return result
+
+@app.get('/icd/<value>')
+async def icdcode(value):
+    print('value: ', value)
+    filtered = df[df['principal_diagnosis_code'] == value]
+    if len(filtered) <= 0:
+        return 'There is nothing here'
+    else: 
+        return filtered.to_json(orient="records")
+
+@app.get('/icd/<value>/sex/<value2>')
+async def icdcode2(value, value2):
+    filtered = df[df['principal_diagnosis_code'] == value]
+    filtered2 = filtered[filtered['sex'] == value2]
+    if len(filtered2) <= 0:
+        return 'There is nothing here'
+    else: 
+        return filtered2.to_json(orient="records")
+
+
